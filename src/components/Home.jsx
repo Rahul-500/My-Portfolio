ff4b4228a2c891787e657bb397ea3021f17c8826
Rahul-500 <rahulspatil500@gmail.com>
2024-04-23 20:38:13 +0530

    Update Home page title

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
     <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
             <div className='flex flex-col justify-center h-full'>
-                <h2 className='text-4xl md:text-5xl font-bold text-rose-200'> <br/> I'm a MERN STACK Developer..!!</h2>
+                <h2 className='text-4xl md:text-5xl font-bold text-rose-200'> <br/> I'm a  Software Engineer..!!</h2>
                 <p className='text-gray-200 py-4'>Hello World..!! My name is Rahul Patil currently pursuing my Computer Science  degree from Gogte Institute of Technology. I am currently in my third year. I am also an active Coder on different Platforms like 
                 <a className='text-amber-400' target='_blank' rel='noreferrer' href="https://leetcode.com/rahul_patil_33/"> ' Leetcode '</a> <a className='text-emerald-400' target='_blank' rel='noreferrer' href="https://auth.geeksforgeeks.org/user/rahulspatil500/practice"> ' GeeksForGeeks '</a> ..!!</p>
 
