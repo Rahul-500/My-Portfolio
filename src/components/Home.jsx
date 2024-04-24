22344e5592e828213c42e1db7699290a9f362035
Rahul-4480 <rahul.int24@infraspec.dev>
2024-04-24 22:14:30 +0530

    Update profile picture

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,46 @@
-import React from 'react'
-import Rahul from '../assets/Rahul.jpg'
-import {BsArrowRightCircleFill} from 'react-icons/bs'
-import { Link } from 'react-scroll'
+import React from 'react';
+import Rahul from '../assets/Rahul.jpg';
+import { BsArrowRightCircleFill } from 'react-icons/bs';
+import { Link } from 'react-scroll';
 
 const Home = () => {
-  return (
-    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
-        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
-            <div className='flex flex-col justify-center h-full'>
-                <h2 className='text-4xl md:text-5xl font-bold text-rose-200'> <br/> I'm a  Software Engineer..!!</h2>
-                <p className='text-gray-200 py-4'>Hello World..!! My name is Rahul Patil currently pursuing my Computer Science  degree from Gogte Institute of Technology. I am currently in my third year. I am also an active Coder on different Platforms like 
-                <a className='text-amber-400' target='_blank' rel='noreferrer' href="https://leetcode.com/rahul_patil_33/"> ' Leetcode '</a> <a className='text-emerald-400' target='_blank' rel='noreferrer' href="https://auth.geeksforgeeks.org/user/rahulspatil500/practice"> ' GeeksForGeeks '</a> ..!!</p>
-
-                <div>
-                    <Link to='projects' smooth duration={600} className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
-                        My Projects..!!
-                        <span className='group-hover:rotate-90 duration-300'>
-                            <BsArrowRightCircleFill size={25} className='ml-1'/>
-                        </span>
-                    </Link>
+    return (
+        <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
+            <div className='max-w-screen-xl mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row md:space-x-8'>
+                <div className='flex flex-col justify-center h-full'>
+                    <h2 className='text-4xl md:text-5xl font-extrabold text-rose-200'>
+                        I'm a Software Engineer..!!
+                    </h2>
+                    <p className='text-gray-200 py-4 text-lg leading-relaxed lg:mt-2'>
+                        As a passionate and driven soon-to-be Computer Science graduate, I am deeply committed to the constantly-evolving field of
+                        Software Development. I am seeking a challenging role where I can collaborate with like-minded professionals to create
+                        innovative solutions and drive success. Team player with an eye for detail
+                    </p>
+                    <div>
+                        <Link
+                            to='projects'
+                            smooth
+                            duration={600}
+                            className='group text-white w-fit px-6 py-3 my-1 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer hover:scale-105 transition-transform duration-300 lg:mt-2'
+                        >
+                            My Projects!
+                            <span className='group-hover:rotate-90 ml-2'>
+                                <BsArrowRightCircleFill size={25} />
+                            </span>
+                        </Link>
+                    </div>
                 </div>
-            </div>
-            <div>
-                <img src={Rahul} alt="my profile" className='rounded-2xl mx-auto w-2/3 md:w-200'/>
+                <div>
+                    <img
+                        src={Rahul}
+                        alt="my profile"
+                        className='rounded-full border-4 border-cyan-500 shadow-xl shadow-cyan-500/50 mx-auto w-2/3 md:w-2/3 lg:w-5/6 animate-fadeIn'
+                    />
+                </div>
+
             </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Home
+export default Home;
