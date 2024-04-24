97ab812e4bf92b183ba86cc15de8a6bb30a4619d
Rahul-4480 <rahul.int24@infraspec.dev>
2024-04-24 22:12:12 +0530

    Remove about option from navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,22 +13,18 @@ const Navbar = () => {
         },
         {
             id: 2,
-            link: 'about',
-        },
-        {
-            id: 3,
             link: 'experience',
         },
         {
-            id: 4,
+            id: 3,
             link: 'projects',
         },
         {
-            id: 5,
+            id: 4,
             link: 'skills',
         },
         {
-            id: 6,
+            id: 5,
             link: 'contact',
         },
         
@@ -37,7 +33,7 @@ const Navbar = () => {
   return (
     <div className='flex justify-between items-center w-full h-20 text-white bg-black px-4 fixed'>
         <div>
-            <h1 className='text-3xl font-signature ml-7 md:text-5xl'>Rahul</h1>
+        <h1 className='text-3xl font-signature ml-7 md:text-5xl'>Rahul Patil</h1>
         </div>
 
         <ul className='hidden md:flex mr-20'>
